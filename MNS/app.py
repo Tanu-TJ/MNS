2c495ad1c2240dbcc76f88474847bf1f1a8c43b5
Tanmay Jain <tanmay08jain@gmail.com>
2025-05-08 15:23:04 +0530

    Update app.py

--- MNS/app.py
+++ MNS/app.py
@@ -1,6 +1,4 @@
-import os
 from flask import Flask, render_template
-from waitress import serve
 
 # Create the Flask application
 app = Flask(__name__)
@@ -22,6 +20,5 @@ def result(answer1, answer2):
     elif answer1 == 'no' and answer2 == 'no':
         return render_template('result_2.html')  # Show Result 4
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use the port from environment or default to 5000
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#    app.run(host='0.0.0.0', port=8080, debug=True)  # Optional for local dev only
